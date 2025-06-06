7841abff8437b48fd7ec4822c4c7f3b39deadd02
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-06-06 16:56:08 -0500

    initial commit

---
+++ projects/hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// The ! after println refers to a macro and not a function.
+// rustc is fine for compiling small programs, but use cargo for real world programs.
+// In cmd, the 'dir /B' command displays only the names of the files.
+// In Rust, packages of code are called crates.  
