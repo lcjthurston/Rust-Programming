07ecd654add2081ec0dbdb2da1020ff2ff7897bb
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-06-07 22:42:09 -0500

    added variables folder and ch 3 sect names

---
+++ projects/Chapter_3/3.3_Functions.rs
@@ -0,0 +1,3 @@
+/*
+Functions are prevalent in Rust code. You’ve already seen one of the most important functions in the language: the main function, which is the entry point of many programs.
+*/
---
+++ projects/Chapter_3/3.4_Comments.rs
@@ -0,0 +1,1 @@
+/*All programmers strive to make their code easy to understand, but sometimes extra explanation is warranted. In these cases, programmers leave comments in their source code that the compiler will ignore but people reading the source code may find useful.*/
